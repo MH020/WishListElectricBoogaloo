ca43f7d1882464b1d89fe0ff88b927df3d28dad4
MH020 <166133291+MH020@users.noreply.github.com>
2024-10-29 09:15:47 +0100

    Initial commit

---
+++ src/main/java/org/example/wishlistelectricboogaloo/WishListElectricBoogalooApplication.java
@@ -0,0 +1,13 @@
+package org.example.wishlistelectricboogaloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListElectricBoogalooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishListElectricBoogalooApplication.class, args);
+    }
+
+}
