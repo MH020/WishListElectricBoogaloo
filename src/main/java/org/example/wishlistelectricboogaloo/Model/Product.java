a3fc133a5e0cbd3262889ed984a9779b462029ba
jazzyzoney <jaqtash22@gmail.com>
2024-11-05 14:44:56 +0100

    setters and getters

--- src/main/java/org/example/wishlistelectricboogaloo/Model/Product.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Model/Product.java
@@ -1,4 +1,44 @@
 package org.example.wishlistelectricboogaloo.Model;
 
 public class Product {
+    int productId;
+    String productName;
+    String productDescription;
+    double productPrice;
+    //int marketId;
+
+    //Getters
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    //Setters
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
 }
--- src/main/java/org/example/wishlistelectricboogaloo/Repository/MarketRepository.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Repository/MarketRepository.java
@@ -1,10 +1,16 @@
 package org.example.wishlistelectricboogaloo.Repository;
 
+import org.example.wishlistelectricboogaloo.Model.Product;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public class MarketRepository {
 
-    public List showAllProductsFromMarket() {
+    //show market that user is connected to by marketId
+    public List<Product> getAllProducts() {
+        String sql = "SELECT * FROM Product WHERE city = ?";
 
     }
 }
