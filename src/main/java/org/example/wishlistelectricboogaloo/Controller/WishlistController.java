3f3103904675248b71d439b5b57c33072bcfcb88
MH020 <166133291+MH020@users.noreply.github.com>
2024-11-05 13:47:27 +0100

    added post and get method to controller

--- src/main/java/org/example/wishlistelectricboogaloo/Controller/WishlistController.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Controller/WishlistController.java
@@ -1,4 +1,34 @@
 package org.example.wishlistelectricboogaloo.Controller;
 
+import org.example.wishlistelectricboogaloo.Model.Wishlist;
+import org.example.wishlistelectricboogaloo.Service.WishlistService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class WishlistController {
+
+    private final WishlistService wishlistService;
+
+    public WishlistController(WishlistService wishlistService) {
+        this.wishlistService = wishlistService;
+    }
+    @GetMapping("/addWishList")
+    public String addWishList(Model model) {
+        model.addAttribute("wishlist", new Wishlist());
+        return "addWishList";
+
+    }
+
+    @PostMapping("/addWishList")
+    public String addWishList(Wishlist wishlist) {
+        wishlistService.createWishlist(wishlist);
+        return "redirect:/addWishList";
+    }
+
+
+
+
 }
