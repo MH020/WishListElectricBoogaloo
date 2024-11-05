9d0a50ab10d2614cdf26c5cbf803155a8e6dc2be
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-11-05 14:00:32 +0100

    alle endpoints er på

--- src/main/java/org/example/wishlistelectricboogaloo/Controller/ProfileController.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Controller/ProfileController.java
@@ -1,10 +1,32 @@
 package org.example.wishlistelectricboogaloo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/homepage/{profileID}")
 public class ProfileController {
+
+    @GetMapping("")
+    public String getMyHomepage(){
+        return "myHomepage";
+    }
+
+    @GetMapping("/market")
+    public String getMarketPage(){
+        return "market";
+    }
+
+    @PostMapping("/logout")
+    public String postLogout(){
+        return "redirect: loginPage";
+    }
+
+    @PostMapping("/deleteAccount")
+    public String postDeleteAccount(){
+        return "redirect: loginPage";
+    }
 
 }
--- src/main/java/org/example/wishlistelectricboogaloo/Controller/WishlistController.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Controller/WishlistController.java
@@ -1,4 +1,33 @@
 package org.example.wishlistelectricboogaloo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/homepage/{profileID}/wishlist")
 public class WishlistController {
+
+    @GetMapping("/view/{wishlistID}")
+    public String getWishlist(){
+        return "myWishlist";
+    }
+
+    @PostMapping("/delete/{wishlistID}")
+    public String deleteWishlist(){
+        return "redirect: myHomepage";
+    }
+
+    @PostMapping("/update/{wishlistID}/removeWish/{productID}")
+    public String updateWishlistRemoveProduct(){
+        return "redirect: myWishlist";
+    }
+
+    @PostMapping("/update/{wishlistID}/addWish")
+    public String updateWishlistAddProduct(){
+        return "redirect: market";
+    }
+
+    //get and post: create new wishlist
 }
