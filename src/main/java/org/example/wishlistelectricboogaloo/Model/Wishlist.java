2ffb37832147bb3be6e7c47e29b83a9999c5efa1
MH020 <166133291+MH020@users.noreply.github.com>
2024-11-05 10:52:20 +0100

    leger med wishlist, og har lavet test data til h2en

--- src/main/java/org/example/wishlistelectricboogaloo/Model/Wishlist.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Model/Wishlist.java
@@ -1,5 +1,22 @@
 package org.example.wishlistelectricboogaloo.Model;
 
 public class Wishlist {
-    
+   private int id;
+   private String City;
+
+   public Wishlist() {
+   }
+
+   public int getId() {
+      return id;
+   }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCity() {
+        return City;
+    }
+    public void setCity(String City) {
+        this.City = City;
+    }
 }
--- src/main/java/org/example/wishlistelectricboogaloo/Repository/WishlistRepository.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Repository/WishlistRepository.java
@@ -1,4 +1,27 @@
 package org.example.wishlistelectricboogaloo.Repository;
 
+import org.example.wishlistelectricboogaloo.ConnectionManager;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@Repository
 public class WishlistRepository {
+    private final Connection conn;
+
+    public WishlistRepository(ConnectionManager connectionManager) {
+        this.conn = connectionManager.getConnection();
+    }
+
+    //create a wishlist
+    /*public createWishlist( String name) {
+
+    String SQlInsertWishlist ="insert into wishlist (name,) values(?)";
+    }
+    Public GetUserid(){
+        String SQLGetUser = "SELECT FROM profile WHERE NAME = ?";
+        try (PreparedStatement statement =conn.prepareStatement(Statement.RETURN_GENERATED_KEYS))
+    }*/
 }
