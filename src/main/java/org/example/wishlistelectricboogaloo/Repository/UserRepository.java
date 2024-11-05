c4aee3ea96af5966234de439fee1a57e6f14ef6a
MH020 <166133291+MH020@users.noreply.github.com>
2024-11-05 18:25:33 +0100

    added aurthinticator

--- src/main/java/org/example/wishlistelectricboogaloo/Repository/UserRepository.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Repository/UserRepository.java
@@ -1,6 +1,9 @@
 package org.example.wishlistelectricboogaloo.Repository;
 import org.example.wishlistelectricboogaloo.ConnectionManager;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @org.springframework.stereotype.Repository
 public class UserRepository {
@@ -22,5 +25,25 @@ public class UserRepository {
     //delete Method
     public void deleteUser() {
     }
+
+    //authenticate method
+    public boolean authenticateUser(String Username, String Password) {
+        String SQLcheck = "SELECT * FROM User WHERE Username = ? AND Password = ?";
+
+        try (PreparedStatement preparedStatement = conn.prepareStatement(SQLcheck)) {
+            preparedStatement.setString(1, Username);
+            preparedStatement.setString(2, Password);
+
+            //sees if there is a resultSet in the database that matches the username and password entered by the user and returns true if there is
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSet.next();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
 
 }
