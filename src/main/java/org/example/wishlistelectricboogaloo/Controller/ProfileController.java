4f002f9fef1555ff1a8f719fd81747c374cd9fa2
jazzyzoney <jaqtash22@gmail.com>
2024-11-06 12:26:06 +0100

    showLocalMarket og test til

--- src/main/java/org/example/wishlistelectricboogaloo/Controller/ProfileController.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Controller/ProfileController.java
@@ -1,10 +1,31 @@
 package org.example.wishlistelectricboogaloo.Controller;
 
+import org.springframework.ui.Model;
+import org.example.wishlistelectricboogaloo.Model.Product;
+import org.example.wishlistelectricboogaloo.Service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/homepage/{profileID}")
 public class ProfileController {
+    private final UserService userService;
 
+    public ProfileController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/market")
+    public String getMarket(int marketId, Model model) {
+
+        // Retrieve products by marketId
+        List<Product> products = userService.getAllProducts(marketId);
+        // Add the list of products to the model
+        model.addAttribute("products");
+        // Return the view name for the "market" page
+        return "market";
+    }
 }
--- src/main/java/org/example/wishlistelectricboogaloo/Service/UserService.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Service/UserService.java
@@ -1,5 +1,24 @@
 package org.example.wishlistelectricboogaloo.Service;
 
+import org.example.wishlistelectricboogaloo.Model.Product;
+import org.example.wishlistelectricboogaloo.Repository.MarketRepository;
+import org.example.wishlistelectricboogaloo.Repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
-    
+    private final UserRepository userRepository;
+    private final MarketRepository marketRepository;
+
+    public UserService(UserRepository userRepository, MarketRepository marketRepository) {
+        this.userRepository = userRepository;
+        this.marketRepository = marketRepository;
+    }
+
+    //show local market
+    public List<Product> getAllProducts(int marketId) {
+        return marketRepository.getAllProducts(marketId);
+    }
 }
