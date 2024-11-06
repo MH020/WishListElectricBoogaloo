e664f2c557523e7df8ebb9e98a35e92e472df365
carobraaa <166133374+carobraaa@users.noreply.github.com>
2024-11-06 11:52:58 +0100

    Setter and getters på begge classer.

--- src/main/java/org/example/wishlistelectricboogaloo/Model/User.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Model/User.java
@@ -1,9 +1,60 @@
 package org.example.wishlistelectricboogaloo.Model;
 
+import java.util.List;
+
 public class User {
     private int id;
     private String username;
     private String Password;
     private String Email;
     private String PhoneNumber;
+    private List<Wishlist> wishlists; // skal vi have denne under vores profile?
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public List<Wishlist> getWishlists() {
+        return wishlists;
+    }
+
+    public void setWishlists(List<Wishlist> wishlists) {
+        this.wishlists = wishlists;
+    }
 }
--- src/main/java/org/example/wishlistelectricboogaloo/Model/Wishlist.java
+++ src/main/java/org/example/wishlistelectricboogaloo/Model/Wishlist.java
@@ -1,29 +1,49 @@
 package org.example.wishlistelectricboogaloo.Model;
 
+import java.util.List;
+
 public class Wishlist {
    private int id;
    private int profileId;
    private String name;
+   private List<Product> products;
 
    public Wishlist() {
    }
 
    public int getId() {
       return id;
+
    }
     public void setId(int id) {
         this.id = id;
+
     }
     public int getProfileId() {
         return profileId;
+
     }
     public void setProfileId(int profileId) {
-        this.profileId = profileId;
+       this.profileId = profileId;
     }
     public String getName() {
-        return name;
+       return name;
+    }
+
+    public void setName(String name) {
+       this.name = name;
+    }
+
+    public List<Product> getProducts() {
+       return products;
+    }
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+    public String getCity() {
+       return name;
     }
     public void setName(String City) {
-        this.name = City;
+       this.name = City;
     }
 }
