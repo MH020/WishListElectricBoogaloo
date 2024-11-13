1d4c82a61adf6f5d2a746d0d2796b5fca4af7bb8
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-11-13 14:59:45 +0100

    test profile repo opdateret

--- src/test/java/org/example/wishlistelectricboogaloo/Repository/ProfileRepositoryTest.java
+++ src/test/java/org/example/wishlistelectricboogaloo/Repository/ProfileRepositoryTest.java
@@ -1,13 +1,27 @@
 package org.example.wishlistelectricboogaloo.Repository;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.example.wishlistelectricboogaloo.Model.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
+
+@SpringBootTest
+// Uses active profile to make sure we are hooked to database
+@ActiveProfiles("test")
+// @SQL ensures that h2 is reset for usage
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:schema.sql")
 class ProfileRepositoryTest {
 
     Profile profile;
+
+    @Autowired
+    ProfileRepository profileRepository;
+
+
 
     @Test
     void saveUser() {
@@ -15,23 +29,22 @@ class ProfileRepositoryTest {
         profile.setPassword("test");
         profile.setUsername("test");
         profile.setEmail("test@mail.com");
-        profile.setPhoneNumber(12341234);
+        profile.setPhoneNumber("12341234");
+        profile.setId(3); //two instances of profiles already exist.
 
+        int actualProfile_id = profileRepository.saveUser(profile);
+        int expectedProfile_id = profile.getId();
 
-
-
-    }
-
-    @Test
-    void readUser() {
-    }
-
-    @Test
-    void updateUser() {
+        assertEquals(expectedProfile_id, actualProfile_id);
     }
 
     @Test
     void deleteUser() {
+        profile = new Profile();
+        profile.setId(3);
+        profileRepository.deleteUser(profile.getId());
+
+        //assertNull(profile);
     }
 
     @Test
